6e4aeb35043720bb8a81093db3febc71396282a0
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-03-27 00:01:25 +0530

    Compute Quotient and Remainder

---
+++ src/Streamliners/Task0/P05.java
@@ -0,0 +1,16 @@
+package Streamliners.Task0;
+
+// Program to Compute Quotient and Remainder
+
+public class P05
+{
+    public static void main(String[] args)
+    {
+        int dividend = 45;
+        int divisor = 4;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.print("Quotient = " + quotient + "\nRemainder = " + remainder);
+    }
+}
