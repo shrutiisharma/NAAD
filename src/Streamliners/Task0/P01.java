a10c4632406fffdde261a927bc36be766a66b4f0
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-03-26 21:18:13 +0530

    Initial commit

---
+++ src/Streamliners/Task0/P01.java
@@ -0,0 +1,15 @@
+package Streamliners.Task0;
+
+//Program to print an integer (entered by the user)
+
+import java.util.Scanner;
+public class P01
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int number = input.nextInt();
+        System.out.println("You entered: " + number);
+    }
+}
