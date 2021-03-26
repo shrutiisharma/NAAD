14b72f7e7197a49f1e1b798c207b34bd9513004d
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-03-26 22:38:23 +0530

    Input & Print an Integer

--- src/Streamliners/Task0/P01.java
+++ src/Streamliners/Task0/P01.java
@@ -5,7 +5,7 @@ package Streamliners.Task0;
 import java.util.Scanner;
 public class P01
 {
-    public static void main(String[] args)
+    public static void main (String[] args)
     {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter an integer:");
