ed9da8ebaaafe7115fd7be19e388194deb8c19b9
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-03-26 22:48:24 +0530

    Multiply 2 FloatingPoint No.s

---
+++ src/Streamliners/Task0/P03.java
@@ -0,0 +1,14 @@
+package Streamliners.Task0;
+
+//Program to Multiply two Floating Point Numbers
+
+public class P03
+{
+    public static void main(String[] args)
+    {
+        float n1 = 5.4f;
+        float n2 = 4.3f;
+        float product = n1 * n2;
+        System.out.println("The product is " + product);
+    }
+}
