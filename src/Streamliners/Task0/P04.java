c6ed8dbc7e604245ff42370e9f6b5f2480e0554e
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-03-26 23:53:10 +0530

    ASCII Value of a character

---
+++ src/Streamliners/Task0/P04.java
@@ -0,0 +1,17 @@
+package Streamliners.Task0;
+
+// Program to Find ASCII Value of a character
+
+public class P04
+{
+    public static void main(String[] args)
+    {
+        char ch = 's';
+        int ascii_value = ch;
+
+        int cast_value = (int)ch;
+
+        System.out.println("Ascii value of " + ch + " is " + ascii_value);
+        System.out.println("Ascii value of " + ch + " is " + cast_value);
+    }
+}
