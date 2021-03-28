74ad8c758a6ee79e3301268c6386407b54a1129d
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-03-28 23:23:17 +0530

    Roots of Quadratic Equation

--- src/Streamliners/Task0/_2_TypeConversion/P01.java
+++ src/Streamliners/Task0/_2_TypeConversion/P01.java
@@ -6,6 +6,18 @@ public class P01
 {
     public static void main(String[] args)
     {
+        String a = "23", b = "hello", c = "56";
 
+        //method 1
+        int n1 = Integer.parseInt(a);
+        System.out.println("n1 = " + n1);
+
+        //method 2 ->shows error
+        //int n2 = Integer.parseInt(b);
+        //System.out.println("n2 = " + n2);
+
+        //method 3
+        //int n3 = Integer.valueOf(c);
+        //System.out.println("n3 = " + n3);
     }
 }
