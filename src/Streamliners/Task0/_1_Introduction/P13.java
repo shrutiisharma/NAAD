443405412fd2f04ab2258fbf301ad0dd0d7a9514
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-03-28 23:27:52 +0530

    Rounding Off a No.

---
+++ src/Streamliners/Task0/_1_Introduction/P13.java
@@ -0,0 +1,12 @@
+package Streamliners.Task0._1_Introduction;
+
+//Program to Round a Number to n Decimal Places
+
+public class P13
+{
+    public static void main(String[] args)
+    {
+        double num = 55.998751236;
+        System.out.format("%.4f", num);
+    }
+}
