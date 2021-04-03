77f4010f85fbde9bbf7c7187daf15cd01e45808d
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-04-03 08:22:10 +0530

    Display Alphabets using loop

---
+++ src/Streamliners/Task0/_3_DecisionMakingAndLoops/P03.java
@@ -0,0 +1,12 @@
+package Streamliners.Task0._3_DecisionMakingAndLoops;
+
+//Program to Display Alphabets (A to Z) using loop
+
+public class P03
+{
+    public static void main(String[] args)
+    {
+        for (char i = 'A'; i <= 'Z'; i++)
+            System.out.print(i + " ");
+    }
+}
