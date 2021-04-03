c1b2a0c1784d5de050602457a00f06473b4525a8
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-04-03 09:33:00 +0530

    Display Fibonacci Series

--- src/Streamliners/Task0/_3_DecisionMakingAndLoops/P02.java
+++ src/Streamliners/Task0/_3_DecisionMakingAndLoops/P02.java
@@ -7,13 +7,13 @@ public class P02
     public static void main(String[] args)
     {
         int n = 20, t1 = 0, t2 = 1;
-        System.out.println(t1 + " " + t2 + " ");
-        for (int i = 0; i <= n; i++)
+        System.out.print(t1 + " " + t2 + " ");
+        for (int i = 0; i < n; i++)
         {
             int sum = t1 + t2;
-            System.out.println(sum + " ");
-            t1++;
-            t2++;
+            System.out.print(sum + " ");
+            t1 = t2;
+            t2 = sum;
         }
     }
 }
