f1f0512f3fe883c86aef23a83db999e6e7f2a8cc
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-04-03 20:49:17 +0530

    Compare Strings

---
+++ src/Streamliners/Task0/_5_Strings/P02.java
@@ -0,0 +1,17 @@
+package Streamliners.Task0._5_Strings;
+
+//Program to Compare Strings
+
+public class P02
+{
+    public static void main(String[] args)
+    {
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        if(s1 == s2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
