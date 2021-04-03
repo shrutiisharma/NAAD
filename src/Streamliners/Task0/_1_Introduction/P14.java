fc511876cfdebc9a0513816b9c8f5f6f7be9185f
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-04-03 09:03:43 +0530

    Empty or Null String

--- src/Streamliners/Task0/_1_Introduction/P14.java
+++ src/Streamliners/Task0/_1_Introduction/P14.java
@@ -6,6 +6,19 @@ public class P14
 {
     public static void main(String[] args)
     {
+        String s1 = null;
+        String s2 = "";
+        String s3 = "  ";
 
+        System.out.println("s1 is " + nullOrEmpty(s1));
+        System.out.println("s2 is " + nullOrEmpty(s2));
+        System.out.println("s3 is " + nullOrEmpty(s3));
+    }
+
+    public static String nullOrEmpty(String str)
+    {
+        if (str == null)            return "NULL";
+        else if (str.isEmpty())     return "EMPTY";
+        else return "Neither null nor empty";
     }
 }
