f8c9beae2db7cec65605bf00a8bcc54d36dd63f2
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2021-04-03 10:26:50 +0530

    half pyramid using *

---
+++ src/Streamliners/Task0/_3_DecisionMakingAndLoops/P11.java
@@ -0,0 +1,22 @@
+package Streamliners.Task0._3_DecisionMakingAndLoops;
+
+//Java Code To Create Pyramid and Pattern
+//Program to print half pyramid using *
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+
+public class P11
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
